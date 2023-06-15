86cc44365e4201bee320ef1409952a3c7ae6acdf
inezacha <inezachanny2@gmail.com>
2023-06-15 08:25:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,6 @@
 */
 int main(void)
 {
-         puts("\"Programming is building a multilingual puzzle");
-         
+         puts("\"Programming is building a multilingual puzzle");         
          return (0);
 }
