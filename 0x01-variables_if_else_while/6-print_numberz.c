a545959ba65f1afec8df58be74b20fe85835530f
inezacha <inezachanny2@gmail.com>
2023-06-17 04:58:48 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,11 +2,12 @@
 /**
  * main - Prints numbers form 0 to 9.
  *
- * Retrun: Always 0 (Success)
+ * Retrun: Always 0(Success)
  */
 
-int main(void) {
+int main(void)
 
+{
 	int i;
 
 	for (i = 48; i < 58; i++)
