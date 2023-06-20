4797c90768e3c122fd21ef379e7788d6f8eb5665
inezacha <inezachanny2@gmail.com>
2023-06-20 11:46:07 -0700

    101-natural

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,13 +7,13 @@
  */
 void print_times_table(int n)
 {
-	int i, j, k;
+	int i, j;
 
 	if (n > 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			_putchar(k + '0');
+			_putchar('0');
 			for (j = 1; j <= n; j++)
 				putformat(i * j);
 			_putchar('\n');
@@ -21,6 +21,11 @@ void print_times_table(int n)
 	}
 }
 
+/**
+ * putformat - formatted characters to output
+ * @n: number to format
+ * Return: nothing
+ */
 void putformat(int n)
 {
 	if (n <= 9)
