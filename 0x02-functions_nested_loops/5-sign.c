600d57d1f8303cc9dd2f776838730f7307505935
inezacha <inezachanny2@gmail.com>
2023-06-20 11:58:31 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,31 @@
 #include "main.h"
 
 /**
- *print_sign - Prints the sign of a number 
- * * @n: The number to be checked
- * Return: 1 for positive num, -1 for negative num or zero for anyting else
+ * print_sign - Check Holberton
+ * @n: An input number
+ * Description: Tohis function prints the sign of the number
+ * Return: 1 if number is positive, o if number is 0, or
+ * -1 if number is negative
  */
-
 int print_sign(int n)
-
 {
+	int value;
+
 	if (n > 0)
 	{
-		_putchar(43);
-		return (1);
+		value = 1;
+		_putchar('+');
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		_putchar(45);
-		return (-1);
+		value = 0;
+		_putchar('0');
 	}
 	else
 	{
-		_putchar(48);
-		return (0);
+		value = -1;
+		_putchar('-');
 	}
+
+	return (value);
 }
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ *_abs - Check Holberton
+ * @r: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number r
  */
-int main(void)
+int _abs(int r)
 {
-  int r;
-
-  r = _abs(-1);
-
-  printf("%d\n", r);
-  r = _abs(0);
-  printf("%d\n", r);
-  r = _abs(1);
-  printf("%d\n", r);
-  r = _abs(-98);
-  printf("%d\n", r);
-  return (0);
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
 }
