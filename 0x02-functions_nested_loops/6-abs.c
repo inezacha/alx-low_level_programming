f3090efb93c835ef9d2cc5e772eda9b0091b9b93
inezacha <inezachanny2@gmail.com>
2023-06-20 09:32:24 -0700

    7-print_last_digit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stlib.h>
+
+/**
+ * _abs - computes the absolute value of an integer.
+ * @c: The number to be computed
+ * Return: Absolute value of number or zero
+ */
+
+int _abs(int c);
+{
+	if (c < 0)
+	{
+	int abs_val;
+
+	r = c * (-1);
+	return (abs_val);
+	}
+	return (c);
+}
