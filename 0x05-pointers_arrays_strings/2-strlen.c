800990d30b9b5b77f2bebb2d714b6aef309b3588
inezacha <inezachanny2@gmail.com>
2023-06-26 03:01:19 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * strlen - returns tje length of a string
+ * @s: string to evaluate
+ *
+ * return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
